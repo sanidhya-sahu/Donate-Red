71c9bb460f9d5167da9b5ebd904892f24a1f6c04
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2024-05-02 23:28:36 +0530

    s

--- frontend/js/logCheck.js
+++ frontend/js/logCheck.js
@@ -6,7 +6,7 @@ window.addEventListener('load',()=>{
             document.getElementById('headLoginButton').style.display = "none"
             document.getElementById('nav').innerHTML += `
             <h5 style="font-family: var(--ff-poppins);color: white;font-weight: 500;font-size: medium;"><a style=" color: white; text-decoration: none; "href='/logout'>Logout</a></h5>&nbsp;&nbsp;&nbsp;
-            <h3 style="font-weight: bold;font-family: var(--ff-poppins);color: white;margin-left: 20px;">Sanidhya</h3>
+            <h3 style="font-weight: bold;font-family: var(--ff-poppins);color: white;margin-left: 20px;">${res.user.name}</h3>
             `
             document.getElementById('regButton').style.display = "none"
             document.getElementById('mainButton').innerHTML = `
