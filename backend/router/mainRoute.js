5cb785b79450ed87c1952e19f10d595d2728b1f3
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2024-05-02 23:14:55 +0530

    s

--- backend/router/mainRoute.js
+++ backend/router/mainRoute.js
@@ -18,7 +18,7 @@ const frontPath = path.join(__dirname, '../../', 'frontend', '/')
 router.use(express.static(frontPath));
 
 router.get(`/`,(req, res) => {
-    res.status(200).sendFile(frontPath + 'Html/index.html')
+    res.status(200).sendFile(frontPath + 'html/index.html')
 })
 
 function isLoggedIn(req, res, next) {
@@ -33,13 +33,13 @@ function isLoggedIn(req, res, next) {
 router.get(`/donate`, isLoggedIn, (req, res) => {
     const user = req.session.user
     if (user.donarReg == false) {
-        res.sendFile(frontPath + `HTML/check2.html`)
+        res.sendFile(frontPath + `html/check2.html`)
     }
     else if(user.donarReg == true){
-        res.sendFile(frontPath + `HTML/already.html`)
+        res.sendFile(frontPath + `html/already.html`)
     }
     else{
-        res.sendFile(frontPath + `HTML/error.html`)
+        res.sendFile(frontPath + `html/error.html`)
     }
 })
 
@@ -48,7 +48,7 @@ router.get(`/logpage`,(req,res)=>{
         res.redirect('/')
     }
     else{
-        res.sendFile(frontPath + `HTML/login.html`)
+        res.sendFile(frontPath + `html/login.html`)
     }
 })
 router.get(`/login`,(req,res)=>{
@@ -56,7 +56,7 @@ router.get(`/login`,(req,res)=>{
         res.redirect('/')
     }
     else{
-        res.sendFile(frontPath + `HTML/login.html`)
+        res.sendFile(frontPath + `html/login.html`)
     }
 })
 router.get(`/register`,(req,res)=>{
@@ -64,26 +64,26 @@ router.get(`/register`,(req,res)=>{
         res.redirect('/')
     }
     else{
-        res.sendFile(frontPath + `HTML/register.html`)
+        res.sendFile(frontPath + `html/register.html`)
     }
 })
 router.get(`/about`,(req,res)=>{
-    res.sendFile(frontPath + `HTML/aboutus.html`)
+    res.sendFile(frontPath + `html/aboutus.html`)
 })
 router.get(`/contact`,(req,res)=>{
-    res.sendFile(frontPath + `HTML/aboutus.html`)
+    res.sendFile(frontPath + `html/aboutus.html`)
 })
 router.get(`/home`,(req,res)=>{
-    res.sendFile(frontPath + `HTML/index.html`)
+    res.sendFile(frontPath + `html/index.html`)
 })
 router.get(`/find`,(req,res)=>{
-    res.sendFile(frontPath + `HTML/find.html`)
+    res.sendFile(frontPath + `html/find.html`)
 })
 router.get(`/error`,(req,res)=>{
-    res.sendFile(frontPath + `HTML/error.html`)
+    res.sendFile(frontPath + `html/error.html`)
 })
 router.get(`/blog`,(req,res)=>{
-    res.sendFile(frontPath + `HTML/blogs.html`)
+    res.sendFile(frontPath + `html/blogs.html`)
 })
 
 
