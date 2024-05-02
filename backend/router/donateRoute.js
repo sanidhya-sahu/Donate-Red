f792d0bb891ed76a0205b41e681332b1aa925ba1
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2024-05-02 19:51:44 +0530

    s

--- backend/router/donateRoute.js
+++ backend/router/donateRoute.js
@@ -25,14 +25,14 @@ function isLoggedIn(req, res, next) {
 router.get(`/donarDetailsSave1`, isLoggedIn, (req, res) => {
     try {
         const detailsOBJ1 = {
-            "donartype": req.query.res.donartype,
-            "medicine": req.query.res.medicine,
-            "lastdonation": req.query.res.lastdonation,
-            "immunizations": req.query.res.immunizations,
-            "Malaria": req.query.res.Malaria,
-            "recieveblood": req.query.res.recieveblood,
-            "Rabies": req.query.res.Rabies,
-            "jaundice": req.query.res.jaundice
+            "donartype": req.query.donartype,
+            "medicine": req.query.medicine,
+            "lastdonation": req.query.lastdonation,
+            "immunizations": req.query.immunizations,
+            "Malaria": req.query.Malaria,
+            "recieveblood": req.query.recieveblood,
+            "Rabies": req.query.Rabies,
+            "jaundice": req.query.jaundice
         }
         req.session.detailsOBJ1 = detailsOBJ1
         res.json({
@@ -47,15 +47,15 @@ router.get(`/donarDetailsSave1`, isLoggedIn, (req, res) => {
 router.get(`/donarDetailsSave2`, isLoggedIn, (req, res) => {
     try {
         const detailsOBJ2 = {
-            "Diabetes": req.query.res.Diabetes,
-            "Cancer": req.query.res.Cancer,
-            "Tuberculosis": req.query.res.Tuberculosis,
-            "asthma": req.query.res.asthma,
-            "liver": req.query.res.liver,
-            "kidney": req.query.res.kidney,
-            "clot": req.query.res.clot,
-            "Heart": req.query.res.Heart,
-            "Allergy": req.query.res.Allergy
+            "Diabetes": req.query.Diabetes,
+            "Cancer": req.query.Cancer,
+            "Tuberculosis": req.query.Tuberculosis,
+            "asthma": req.query.asthma,
+            "liver": req.query.liver,
+            "kidney": req.query.kidney,
+            "clot": req.query.clot,
+            "Heart": req.query.Heart,
+            "Allergy": req.query.Allergy
         }
         req.session.detailsOBJ2 = detailsOBJ2
         res.json({
--- frontend/js/details.js
+++ frontend/js/details.js
@@ -11,6 +11,25 @@ window.addEventListener('load',()=>{
             document.getElementById('bloodGrp').innerText = user.bloodGrp
             document.getElementById('state').innerText = user.state
             document.getElementById('city').innerText = user.city
+            const details1 = response.data.details1
+            const details2 = response.data.details2
+            document.getElementById('firstdonar').innerText = details1.donartype
+            document.getElementById('medicine').innerText = details1.medicine
+            document.getElementById('lastdonate').innerText = details1.lastdonation
+            document.getElementById('immu').innerText = details1.immunizations
+            document.getElementById('malaria').innerText = details1.Malaria
+            document.getElementById('recieveblood').innerText = details1.recieveblood
+            document.getElementById('jaundice').innerText = details1.Rabies
+            document.getElementById('Rabies').innerText = details1.jaundice
+            document.getElementById('Diabetes').innerText = details2.Diabetes
+            document.getElementById('Cancer').innerText = details2.Cancer
+            document.getElementById('Tuberculosis').innerText = details2.Tuberculosis
+            document.getElementById('asthma').innerText = details2.asthma
+            document.getElementById('liver').innerText = details2.liver
+            document.getElementById('kidney').innerText = details2.kidney
+            document.getElementById('clot').innerText = details2.clot
+            document.getElementById('Heart').innerText = details2.Heart
+            document.getElementById('Allergy').innerText = details2.Allergy
         }
         else{
             window.location.href = "/error"
